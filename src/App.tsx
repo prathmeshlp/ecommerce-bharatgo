b2677d6f971ca890dcb51eda9f73ff7188e25899
Prathmesh Mulhar <prathmeshmulhar@gmail.com>
2025-04-28 18:56:42 +0530

    show navbar only to lgged in user

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import Navbar from "./components/Navbar";
@@ -9,25 +9,21 @@ import Register from "./components/Register";
 import MyOrders from "./components/MyOrders";
 import MyAccount from "./components/MyAccount";
 import Sidebar from "./components/Sidebar";
-import { AuthProvider } from "./contexts/AuthContext";
-
+import { useAuth } from "./hooks/useAuth";
 
 const App: React.FC = () => {
+  const { user } = useAuth();
   return (
     <Provider store={store}>
-      <AuthProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/orders" element={<MyOrders />} />
-            <Route path="/account" element={<MyAccount />} />
-          </Routes>
-          <Sidebar />
-        </Router>
-      </AuthProvider>
+      {user && <Navbar />} 
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/orders" element={<MyOrders />} />
+        <Route path="/account" element={<MyAccount />} />
+      </Routes>
+      <Sidebar />
     </Provider>
   );
 };
